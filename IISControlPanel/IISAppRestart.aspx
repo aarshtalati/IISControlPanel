38080f2ace19769079a8add27ad6ada3e807607a
aarshtalati <aarshstalati1989@gmail.com>
2015-01-12 00:14:21 -0500

    Added IISAppRestart Page

---
+++ IISControlPanel/IISAppRestart.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="IISAppRestart.aspx.cs" Inherits="IISControlPanel.IIS" MaintainScrollPositionOnPostback="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title>Restart Application Pool</title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div>
+			<asp:Repeater ID="tblAppPools" runat="server">
+				<HeaderTemplate>
+					<table border="1" width="100%">
+						<thead>
+							<tr>
+								<th>Native GUID</th>
+								<th>Name</th>
+								<th>Status</th>
+								<th>Action
+									<asp:Button ID="reload" runat="server" Text="Refresh" OnClick="LoadAppPools" />
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+				</HeaderTemplate>
+				<ItemTemplate>
+					<tr>
+						<td><%#Eval("NativeGuid")%></td>
+						<td><%#Eval("Name")%></td>
+						<td><%#Eval("Status")%></td>
+						<td>
+							<asp:Button ID="btStart" runat="server" Visible='<%# Eval("Status").ToString() == "Stopped" %>' CommandName='<%# Eval("Name") %>' OnClick="startAppPool" Text="Start" />
+							<asp:Button ID="btStop" runat="server" Visible='<%# Eval("Status").ToString() == "Running" %>' CommandName='<%# Eval("Name") %>' OnClick="stopAppPool" Text="Stop" />
+							<asp:Button ID="btRecycle" runat="server" Visible='<%# Eval("Status").ToString() == "Running" %>' CommandName='<%# Eval("Name") %>' OnClick="recycleAppPool" Text="Recycle" />
+						</td>
+					</tr>
+				</ItemTemplate>
+				<FooterTemplate>
+					</tbody>
+					<tfoot>
+						<tr>
+							<td colspan="4">
+								<a href="Index.aspx">Home</a>
+							</td>
+						</tr>
+					</tfoot>
+				</FooterTemplate>
+			</asp:Repeater>
+
+			<label><b>Status:</b></label><br />
+			<asp:Label ID="lblStatus" runat="server" /><br />
+
+		</div>
+	</form>
+</body>
+</html>
---
+++ IISControlPanel/IISAppRestart.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IISControlPanel.Models;
+
+namespace IISControlPanel
+{
+	public partial class IIS : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Response.Write("Computer Name : " + System.Environment.MachineName + "<br/>");
+
+			if (IsPostBack)
+				return;
+
+			LoadAppPools();
+		}
+
+		protected void LoadAppPools(object sender = null, EventArgs e = null)
+		{
+			tblAppPools.DataSource = GetApplicationPools();
+			tblAppPools.DataBind();
+		}
+
+		public IEnumerable<IISAppPoolRecord> GetApplicationPools()
+		{
+			DirectoryEntry root = GetDirectoryEntry(@"IIS://" + System.Environment.MachineName + "/W3SVC/AppPools");
+			if (root == null)
+				return null;
+
+			var myAppPool = Request.ServerVariables["APP_POOL_ID"];
+
+			var appPoolNames = from DirectoryEntry entry in root.Children
+							   let status = (AppPoolStatus)(new DirectoryEntry(entry.Path)).InvokeGet("AppPoolState")
+							   where
+									string.Equals(myAppPool, entry.Name) == false
+							   select new IISAppPoolRecord { NativeGuid = entry.NativeGuid, Name = entry.Name, Status = status };
+			return appPoolNames;
+		}
+
+		private DirectoryEntry GetDirectoryEntry(string path)
+		{
+			DirectoryEntry root = null;
+			try
+			{
+				root = new DirectoryEntry(path);
+			}
+
+			catch (Exception ex)
+			{
+				Response.Write("Error Getting Root Entry : " + ex.Message + "<br/>");
+			}
+			return root;
+		}
+
+		protected void stopAppPool(object sender, EventArgs e)
+		{
+			Button btStop = sender as Button;
+			if (btStop == null)
+				return;
+
+			string appPoolName = btStop.CommandName;
+			string appPoolPath = @"IIS://" + System.Environment.MachineName + "/W3SVC/AppPools/" + appPoolName;
+			try
+			{
+				DirectoryEntry w3svc = new DirectoryEntry(appPoolPath);
+				var v = w3svc.Name;
+				w3svc.Invoke("Stop", null);
+				LoadAppPools();
+			}
+			catch (Exception ex)
+			{
+				Response.Write(ex.ToString());
+			}
+		}
+
+		protected void startAppPool(object sender, EventArgs e)
+		{
+			Button btStart = sender as Button;
+			if (btStart == null)
+				return;
+
+			string appPoolName = btStart.CommandName;
+			string appPoolPath = @"IIS://" + System.Environment.MachineName + "/W3SVC/AppPools/" + appPoolName;
+			try
+			{
+				DirectoryEntry w3svc = new DirectoryEntry(appPoolPath);
+				w3svc.Invoke("Start", null);
+				LoadAppPools();
+			}
+			catch (Exception ex)
+			{
+				Response.Write(ex.ToString());
+			}
+		}
+
+		protected void recycleAppPool(object sender, EventArgs e)
+		{
+			Button btStart = sender as Button;
+			if (btStart == null)
+				return;
+
+			string appPoolName = btStart.CommandName;
+			string appPoolPath = @"IIS://" + System.Environment.MachineName + "/W3SVC/AppPools/" + appPoolName;
+			try
+			{
+				DirectoryEntry w3svc = new DirectoryEntry(appPoolPath);
+				w3svc.Invoke("Recycle");
+				LoadAppPools();
+			}
+			catch (Exception ex)
+			{
+				Response.Write(ex.ToString());
+			}
+		}
+	}
+}
