e0fea0565344071d56683915b3d6eb5f5ee5eca5
aarshtalati <aarshstalati1989@gmail.com>
2015-01-12 00:11:15 -0500

    Initial Commit

---
+++ IISControlPanel/Index.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="IISControlPanel.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<a href="IISAppRestart.aspx">Manage Application Pools</a>
+</body>
+</html>
---
+++ IISControlPanel/Models/IISAppPoolRecord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IISControlPanel.Models
+{
+	public enum AppPoolStatus
+	{
+		Running = 2,
+		Stopped = 4
+	}
+
+	public class IISAppPoolRecord
+	{
+		public string Name { get; set; }
+		public string NativeGuid { get; set; }
+		public AppPoolStatus Status { get; set; }
+	}
+}
